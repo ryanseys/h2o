041fab0ff353e6994e4c55b0f92fca7ab3d01e4e
Ryan Seys <ryan@ryanseys.com>
2014-07-03 16:09:51 -0700

    Initial Commit

---
+++ h2o/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  h2o
+//
+//  Created by Ryan Seys on 2014-07-03.
+//  Copyright (c) 2014 Ryan Seys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
