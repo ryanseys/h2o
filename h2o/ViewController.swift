b772375b946983080cee3c950a706b56ca98f733
Ryan Seys <ryan@ryanseys.com>
2014-07-08 11:18:09 -0700

    Initial commit

--- h2o/ViewController.swift
+++ h2o/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var plusButton: UIButton
+    @IBOutlet var water: UIView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var glasses = 0
+    
+    @IBAction func addSomeWater(sender: AnyObject) {
+        if(glasses < 8) {
+            plusButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            
+            glasses++
+        
+            var waterHeight = (water.window.frame.height / 8) * CGFloat(glasses)
+            
+            water.frame = CGRectMake(0, (water.window.frame.height / 2) - (waterHeight/2), water.frame.size.width, waterHeight);
+        }
+        else {
+            var alert = UIAlertController(title: "You're hydrated", message: "Go enjoy the day!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+    }
 }
 
